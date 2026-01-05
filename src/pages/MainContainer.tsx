437515ac2ec45bc432d8f412b203d4590e8fc689
Ayush <ayushawasthi2246@gmail.com>
2026-01-05 21:32:10 +0530

    Add layout preference toggle (tabs vs vertical)

--- src/pages/MainContainer.tsx
+++ src/pages/MainContainer.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import React from "react";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import type { ImperativePanelHandle } from "react-resizable-panels";
 import AgreementData from "../editors/editorsContainer/AgreementData";
@@ -13,7 +14,8 @@ import "../styles/pages/MainContainer.css";
 import html2pdf from "html2pdf.js";
 import { Button } from "antd";
 import { MdExpandMore , MdExpandLess  } from "react-icons/md";
-
+import { LuStretchHorizontal } from "react-icons/lu";
+import { PiTabsBold } from "react-icons/pi";
 
 const MainContainer = () => {
   const agreementHtml = useAppStore((state) => state.agreementHtml);
@@ -75,9 +77,39 @@ const MainContainer = () => {
   }));
 
   const [, setLoading] = useState(true);
+  const [layout, setLayout] = useState<"vertical" | "tabs">("vertical");
+  const [activeTab, setActiveTab] = useState("model");
+
   const modelPanelRef = useRef<ImperativePanelHandle>(null);
   const templatePanelRef = useRef<ImperativePanelHandle>(null);
   const dataPanelRef = useRef<ImperativePanelHandle>(null);
+
+  const Panels = [
+    {
+    id: "model",
+    title: "Concerto Model",
+    ref: modelPanelRef,
+    collapsed: isModelCollapsed,
+    toggle: toggleModelCollapse,
+    content: <TemplateModel />,
+  },
+  {
+    id: "template",
+    title: "TemplateMark",
+    ref: templatePanelRef,
+    collapsed: isTemplateCollapsed,
+    toggle: toggleTemplateCollapse,
+    content: <TemplateMarkdown />,
+  },
+  {
+    id: "data",
+    title: "JSON Data",
+    ref: dataPanelRef,
+    collapsed: isDataCollapsed,
+    toggle: toggleDataCollapse,
+    content: <AgreementData />,
+  },
+  ];
 
   const panelMap = {
   model: {
@@ -92,31 +124,34 @@ const MainContainer = () => {
     ref: dataPanelRef,
     collapsed: isDataCollapsed
   },
-};
+  };
 
-const prevCollapsed = useRef({
-  model: isModelCollapsed,
-  template: isTemplateCollapsed,
-  data: isDataCollapsed,
-}); 
-
-useEffect(() => {
-  Object.entries(panelMap).forEach(([key, panel]) => {
-    if (prevCollapsed.current[key] !== panel.collapsed) {
-      panel.ref.current?.resize(panel.collapsed ? collapsedSize : expandedSize);
-    }
-  });
-
-  // Update previous states
-  prevCollapsed.current = {
+  const prevCollapsed = useRef({
     model: isModelCollapsed,
     template: isTemplateCollapsed,
     data: isDataCollapsed,
-  };
-}, [isModelCollapsed, isTemplateCollapsed, isDataCollapsed]);
+  }); 
 
-  const collapsedSize = 6
-  const expandedSize = 33
+  useEffect(() => {
+    Object.entries(panelMap).forEach(([key, panel]) => {
+      if (prevCollapsed.current[key] !== panel.collapsed) {
+        panel.ref.current?.resize(panel.collapsed ? collapsedSize : expandedSize);
+      }
+    });
+
+    // Update previous states
+    prevCollapsed.current = {
+      model: isModelCollapsed,
+      template: isTemplateCollapsed,
+      data: isDataCollapsed,
+    };
+  }, [isModelCollapsed, isTemplateCollapsed, isDataCollapsed]);
+
+  const collapsedSize = 7; 
+  const collapsedCount = [isModelCollapsed, isTemplateCollapsed, isDataCollapsed].filter(Boolean).length; 
+  const expandedCount = 3 - collapsedCount; 
+  const expandedSize = expandedCount > 0 ? (100 - (collapsedCount * collapsedSize)) / expandedCount : 33;
+  const activePanel = Panels.find(p => p.id === activeTab);
 
   return (
     <div className="main-container" style={{ backgroundColor }}>
@@ -128,149 +163,137 @@ useEffect(() => {
               <div className="main-container-editors-panel" style={{ backgroundColor }}>
                 <div className=""
                 style={{
-                  height: 45,
+                  height: 48,
                   padding: "0px 10px",
                   display: "flex",
-                  alignItems: "center"
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  borderBottom: "5px solid #e5e7eb"
                 }}
                 >
                   <SampleDropdown setLoading={setLoading} />
+                  <button
+                  onClick={()=>setLayout(layout === "tabs" ? "vertical" : "tabs" )}
+                  style={{
+                    fontSize: 30,
+                    color: "#216b9cff",
+                    display: "flex",
+                    border: "none",
+                    background: "transparent",
+                    cursor: "pointer"
+                  }}
+                  title={layout === "tabs" ? "Vertical" : "Tab"}> 
+                  {layout === "tabs" ? <LuStretchHorizontal /> : <PiTabsBold />}
+                  </button>
                 </div>
+                
                 <PanelGroup direction="vertical" className="main-container-editors-panel-group">
-                  <Panel ref={modelPanelRef} minSize={collapsedSize} maxSize={90}
-                  >
-                    <div className="main-container-editor-section tour-concerto-model" >
-                      <div className={`main-container-editor-header 
-                      ${backgroundColor === '#ffffff' ? 'main-container-editor-header-light' : 'main-container-editor-header-dark'}`}
-                      
-                      >
-                        {/* Left side */}
-                        <div 
-                        className="main-container-editor-header-left"
-                        style={{
-                          width: "100%",
-                          display: "flex",
-                          justifyContent: "space-between",
-                          alignItems: "center"
-                        }}
+
+                  {layout === "vertical" &&
+                    Panels.map((panel,index)=>(
+                      <React.Fragment key={panel.id}>
+                        <Panel ref={panel.ref} minSize={collapsedSize} maxSize={90}
                         >
-                          <span>Concerto Model</span>
+                          <div className="main-container-editor-section tour-concerto-model" >
+                            <div className={`main-container-editor-header 
+                            ${backgroundColor === '#ffffff' ? 'main-container-editor-header-light' : 'main-container-editor-header-dark'}`}                      
+                            >
+                              <div 
+                              className="main-container-editor-header-left"
+                              style={{
+                                width: "100%",
+                                display: "flex",
+                                justifyContent: "space-between",
+                                alignItems: "center"
+                              }}
+                              >
+                                <span>{panel.title}</span>
+                                <button
+                                  className="collapse-button"
+                                  onClick={panel.toggle}
+                                  style={{ 
+                                    color: textColor, 
+                                    background: 'transparent', 
+                                    border: 'none', 
+                                    cursor: 'pointer',
+                                    fontSize: '25px',
+                                  }}
+                                  title={panel.collapsed ? "Expand" : "Collapse"}
+                                >
+                                  {panel.collapsed ?  <MdExpandMore /> : <MdExpandLess />}
+                                </button>
+                              </div>
+                            </div>
+                            <div className="main-container-editor-content"
+                              style={{
+                                backgroundColor,
+                                opacity: panel.collapsed ? 0 : 1,
+                                transition: "opacity 0.3s ease-in-out"
+                              }}
+                            >
+                              {panel.content}
+                            </div>
+                          </div>
+                        </Panel>
+                        {index !== Panels.length - 1 && (
+                            <PanelResizeHandle className="main-container-panel-resize-handle-vertical" />
+                        )}                        
+                      </React.Fragment>
+                    ))
+                  }
+                  {layout === "tabs" && (
+                    <React.Fragment>
+                      <div
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        background: backgroundColor === '#ffffff' ? "#ffffffff" :"#375a72",
+                        borderBottom: `solid ${backgroundColor === '#ffffff' ? '5px #e3eaf1' : '8px #374151'}`
+                      }}>
+                      {Panels.map((panel,index)=>{  
+                      const dividerindex = activeTab === "model" ? 1 : activeTab === "data" ? 0 : null ;
+                      return(
+                        <React.Fragment key={panel.id}>
                           <button
-                            className="collapse-button"
-                            onClick={toggleModelCollapse}
-                            style={{ 
-                              color: textColor, 
-                              background: 'transparent', 
-                              border: 'none', 
-                              cursor: 'pointer',
-                              fontSize: '25px',
+                            className={`main-container-editors-tab ${activeTab === panel.id ? 
+                              `${backgroundColor === '#ffffff' ? 'main-container-editor-header-light-selected' : 'main-container-editor-header-dark-selected'}` : 
+                              `${backgroundColor === '#ffffff' ? 'main-container-editor-header-light-unselected' : 'main-container-editor-header-dark-unselected'}`}
+                              }`}
+                            onClick={() => setActiveTab(panel.id)}
+                            style={{
+                              padding: "15px 20px",
+                              transition: "all 0.25s ease-in-out",
+                              borderTopLeftRadius: activeTab === "model" ? "0px" : "15px"
                             }}
-                            title={isModelCollapsed ? "Expand" : "Collapse"}
-                          >
-                            {isModelCollapsed ?  <MdExpandMore /> : <MdExpandLess />}
-                          </button>
-                        </div>
-                      </div>
-                      <div className="main-container-editor-content"
-                        style={{
-                          backgroundColor,
-                          opacity: isModelCollapsed ? 0 : 1,
-                          transition: "opacity 0.3s ease-in-out"
-                        }}
-                      >
-                        <TemplateModel />
-                      </div>
-                    </div>
-                  </Panel>
+                          >{panel.title}</button>
 
-                  <PanelResizeHandle className="main-container-panel-resize-handle-vertical" />
-
-                  <Panel ref={templatePanelRef} minSize={collapsedSize} maxSize={90}>
-                    <div className="main-container-editor-section tour-template-mark">
-                      <div className={`main-container-editor-header ${backgroundColor === '#ffffff' ? 'main-container-editor-header-light' : 'main-container-editor-header-dark'}`}>
-                        <div 
-                        className="main-container-editor-header-left"
-                        style={{
-                          width: "100%",
-                          display: "flex",
-                          justifyContent: "space-between",
-                          alignItems: "center"
-                        }}
-                        >                    
-                          <span>TemplateMark</span>
-                          <button
-                            className="collapse-button"
-                            onClick={toggleTemplateCollapse}
-                            style={{ 
-                              color: textColor, 
-                              background: 'transparent', 
-                              border: 'none', 
-                              cursor: 'pointer',
-                              fontSize: '25px',
-                              
-                            }}
-                            title={isTemplateCollapsed ? "Expand" : "Collapse"}
-                          >
-                            {isTemplateCollapsed ?  <MdExpandMore /> : <MdExpandLess />}
-                          </button>
-                        </div>
-                      </div>
+                          {index === dividerindex && (
+                            <span
+                              style={{
+                              display: "inline-block",
+                              width: 2,
+                              height: 15,
+                              background: `${backgroundColor === '#ffffff' ? 'gray' : 'white'}`,
+                            }}/>
+                          )}
+                        </React.Fragment>
+                        )})}
+                      </div> 
+                      <Panel minSize={0} defaultSize={100} >
+                        <div className="main-container-editor-section tour-template-mark">
                         <div className="main-container-editor-content"
-                        style={{
-                          backgroundColor,
-                          opacity: isTemplateCollapsed ? 0 : 1 ,
-                          transition: "opacity 0.3s ease-in-out"
-                        }}
-                        >
-                          <TemplateMarkdown />
-                      </div>
-                    </div>
-                  </Panel>
-
-                  <PanelResizeHandle className="main-container-panel-resize-handle-vertical" />
-
-                  <Panel ref={dataPanelRef} minSize={collapsedSize} maxSize={90}>
-                    <div className="main-container-editor-section tour-json-data">
-                      <div className={`main-container-editor-header ${backgroundColor === '#ffffff' ? 'main-container-editor-header-light' : 'main-container-editor-header-dark'}`}>
-                        <div 
-                        className="main-container-editor-header-left"
-                        style={{
-                          width: "100%",
-                          display: "flex",
-                          justifyContent: "space-between",
-                          alignItems: "center"
-                        }}
-                        >
-                          <span>JSON Data</span>
-                          <button
-                            className="collapse-button"
-                            onClick={toggleDataCollapse}
-                            style={{ 
-                              color: textColor, 
-                              background: 'transparent', 
-                              border: 'none', 
-                              cursor: 'pointer',
-                              fontSize: '25px',
-                              
+                            style={{
+                              backgroundColor,
+                              opacity: isModelCollapsed ? 1 : 1,
+                              transition: "opacity 0.3s ease-in-out"
                             }}
-                            title={isDataCollapsed ? "Expand" : "Collapse"}
                           >
-                            {isDataCollapsed ?  <MdExpandMore /> : <MdExpandLess />}
-                          </button>
+                            {activePanel?.content}
+                          </div>
                         </div>
-                      </div>
-                        <div className="main-container-editor-content" 
-                        style={{
-                          backgroundColor,
-                          opacity: isDataCollapsed ? 0 : 1,
-                          transition: "opacity 0.3s ease-in-out"
-                        }}
-                        >
-                          <AgreementData />
-                        </div>
-                    </div>
-                  </Panel>
-
+                      </Panel>
+                    </React.Fragment>
+                  )}
 
                   {isProblemPanelVisible && (
                     <>
